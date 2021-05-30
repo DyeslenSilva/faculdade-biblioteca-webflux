0db17da2d78454588ce331f7e3f0e89b4460f123
DyeslenSilva <dyeslensilva97@gmail.com>
2021-05-30 20:52:56 -0300

    atualizando componentes...

--- src/main/java/es/faculdade/biblioteca/controller/EmprestimoController.java
+++ src/main/java/es/faculdade/biblioteca/controller/EmprestimoController.java
@@ -1,5 +1,48 @@
 package es.faculdade.biblioteca.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.faculdade.biblioteca.model.Emprestimo;
+import es.faculdade.biblioteca.repo.EmprestimoRepo;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
 public class EmprestimoController {
+	
+	@Autowired
+	private EmprestimoRepo emprestimoRepo;
+	
+	@PostMapping("/emprestimoCad")
+	public Mono<Emprestimo> cadastroEmprestimo(@RequestBody Emprestimo emprestimo){
+		return emprestimoRepo.save(emprestimo);
+	}
+	
+	@GetMapping("/emprestimos")
+	public Flux<Emprestimo> listaEmprestimo(){
+		return emprestimoRepo.findAll();
+	}
+	
+	@GetMapping("/emprestimo/{idEmprestimo}")
+	public Mono<Emprestimo>emprestimoPorID(@RequestAttribute Integer idEmprestimo){
+		return emprestimoRepo.findById(idEmprestimo);
+	}
+	
+	@PutMapping("/emprestimo/{idEmprestimo}")
+	public Mono<Void> atualizarEmprestimo(@RequestAttribute Integer idEmprestimo){
+		return emprestimoRepo.updateEmprestimo(idEmprestimo);
+	}
+	
+	@DeleteMapping("emprestimo/{idEmprestimo}")
+	public Mono<Void> deleteEmprestimo(@RequestAttribute Integer idEmprestimo){
+		return emprestimoRepo.deleteById(idEmprestimo);
+	}
 
 }
---
+++ src/main/java/es/faculdade/biblioteca/controller/LivroController.java
@@ -0,0 +1,46 @@
+package es.faculdade.biblioteca.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.faculdade.biblioteca.model.Livro;
+import es.faculdade.biblioteca.repo.LivroRepo;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class LivroController {
+
+	private LivroRepo livroRepo;
+	
+	@PostMapping("/cadastroLivro")
+	public Mono<Livro> cadastroLivro(@RequestBody Livro livro){
+		return livroRepo.save(livro);
+	}
+	
+	@GetMapping("/livros")
+	public Flux<Livro> listaLivros(){
+		return livroRepo.findAll();
+	}
+	
+	@GetMapping("/livro/{idLivro}")
+	public Mono<Livro> encontrarLivro(@PathVariable Integer idLivro){
+		return livroRepo.findById(idLivro);
+	}
+	
+	@PutMapping("/livro/{idLivro}")
+	public Mono<Void> atualizarLivro(@PathVariable Integer idLivro){
+		return livroRepo.updateLivro(idLivro);
+	}
+	
+	@DeleteMapping("/livro/idLivro")
+	public Mono<Void> deleteLivro(@PathVariable Integer idLivro){
+		return livroRepo.deleteById(idLivro);
+	}
+	
+}
